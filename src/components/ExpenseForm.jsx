5c851cc14323a442a48533f2e55f2046077872ba
Joshua Oladeji <dejijolaoluwa@gmail.com>
2021-08-29 18:27:09 +0100

    wrote the logic for expense form

--- src/components/ExpenseForm.jsx
+++ src/components/ExpenseForm.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/ExpenseForm.css';
 
 const ExpenseForm = (props) => {
@@ -8,20 +8,77 @@ const ExpenseForm = (props) => {
   const maxDateDay = now.getDate().toString().padStart(2, '0');
   const maxDate = maxDateYear + '-' + maxDateMonth + '-' + maxDateDay;
 
+  const [userInputs, setUserInputs] = useState({ expense: '', amount: '', date: '' });
+
+  const inputHandler = (event) => {
+    if (event.target.name === 'expense') {
+      setUserInputs((prevState) => {
+        return {
+          ...prevState,
+          expense: event.target.value
+        }
+      });
+    }
+    else if (event.target.name === 'amount') {
+      setUserInputs((prevState) => {
+        return {
+          ...prevState,
+          amount: event.target.value
+        }
+      });
+    }
+    else if (event.target.name === 'date') {
+      setUserInputs((prevState) => {
+        return {
+          ...prevState,
+          date: event.target.value
+        }
+      });
+    }
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log({
+      ...userInputs,
+      date: new Date(userInputs.date)
+    });
+  }
+
+
   return (
-    <form className="ExpenseForm">
+    <form className="ExpenseForm" onSubmit={(event) => submitHandler(event)}>
       <div className="input-fields">
-        <div className="title">
-          <input type="text" />
-          <p>Title</p>
+        <div className="expense">
+          <input 
+            type="text"
+            name="expense"
+            value={userInputs.expense}
+            onChange={(event) => inputHandler(event)}
+          />
+          <p className="labels">expense</p>
         </div>
         <div className="amount">
-          <input type="number" min="0.01" step="0.01" />
-          <p>Amount</p>
+          <input 
+            type="number"
+            min="0.01"
+            step="0.01"
+            name="amount"
+            value={userInputs.amount}
+            onChange={(event) => inputHandler(event)}
+          />
+          <p className="labels">amount</p>
         </div>
         <div className="date">
-          <input type="date" min="2021-01-01" max={maxDate} />
-          <p>Date</p>
+          <input 
+            type="date"
+            min="2019-01-01"
+            max={maxDate}
+            name="date"
+            value={userInputs.date}
+            onChange={(event) => inputHandler(event)}
+          />
+          <p className="labels">date</p>
         </div>
       </div>
       <div className="submit-btn">
@@ -30,5 +87,5 @@ const ExpenseForm = (props) => {
     </form>
   );
 }
- 
+
 export default ExpenseForm;
--- src/components/NewExpense.jsx
+++ src/components/NewExpense.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../styles/NewExpense.css';
 import ExpenseForm from './ExpenseForm.jsx';
+import Card from './Card.jsx';
 
 const NewExpense = (props) => {
   return (
-    <div className="NewExpense">
+    <Card className="NewExpense">
       <ExpenseForm />
-    </div>
+    </Card>
   );
 }
  
